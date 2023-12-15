e0da646159ccd02826b466d6e5dcb68a4776f34f
Marta <mdorrego@gmail.com>
2023-12-15 12:09:55 +0100

    Ej1 finalizado

--- pruebas.py
+++ pruebas.py
@@ -1,4 +1,4 @@
-def desplaza_elementos(cadena_lista, num):
+""" def desplaza_elementos(cadena_lista, num):
     #separamos la lista introducida como cadena en elementos usando el separador por defecto " "
     lista = cadena_lista.split()
     #con slice vamos a coger los elementos que hay que mover empezando por la derecha (desplazamiento negativo)
@@ -13,7 +13,7 @@ desplazamiento= int(input('Dime el desplazamiento: '))
 
 print (desplaza_elementos(elementos,desplazamiento))
 
-""" #coge los n últimos que se hayan pedido
+#coge los n últimos que se hayan pedido
 y = x[desplazamiento*(-1):]
 #y= x[:desplazamiento]
 print(y)
@@ -22,3 +22,17 @@ z = x[:desplazamiento*(-1)]
 print(z)
 #juntamos lista de nuevo intercambiándolos
 print (y+z) """
+
+""" def multiplos(divisor, lista):
+    # DONE: completar
+    lista_multiplos= []
+    for x in range(len(lista)):
+        if lista[x]%divisor==0 :
+            lista_multiplos.append(lista[x])
+    return lista_multiplos
+
+# Para probar
+print(multiplos(3, [1,2,3,4,5,6,7,8,9,10])) # Debe mostrar [3, 6, 9]
+print(multiplos(5, [1,2,3,4,5,6,7,8,9,10])) # Debe mostrar [5, 10] """
+
+print ("hola"*2)
