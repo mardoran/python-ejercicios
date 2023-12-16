172c1199b5e367d03ef7ceebe945e9899666647e
Marta <mdorrego@gmail.com>
2023-12-16 09:25:25 +0100

    Ej.2.2 Corregido

--- pruebas2.py
+++ pruebas2.py
@@ -11,24 +11,40 @@ print (sin_duplicados([1,3,4,3,5,5,2,1,3,2,7,8,5]))
 
 print (sin_duplicados(["verde","azul","gris","azul","rojo","negro","rojo"])) """
 
+# Method : Using loop This task can be performed in brute force manner using loops. 
+# In this, we just iterate the list of list using loop and check for the already presence of element, 
+# and append in case it’s new element, and construct a non-duplicate matrix. 
+
 # #Función que recibe una matriz (una lista de listas) y devuelve otra eliminando sus duplicados
-# lista = [[1,2,3], [4,1,5], [1,2,3], [5,2,9]]
-# # # #
-# # # # The matrix is magically updated here.
-# # # #
-# # # print (temps[1][2])
-# lista_nueva =[]
-# matriz_nueva =[]
-# for row in range(len(lista)):
-#     for elem in range(lista[row]): #sacamos los elementos de la lista y tenemos que compararlos con los elementos de la nueva mía
-#         for row_nueva in range(len(matriz_nueva)): #tendremos que recorrer todos los elementos de mi nueva matriz
-#             if elem not in row_nueva: #recorremos toda nuestra nueva matriz
+# initialize list 
+matriz = [[5, 6, 8], [8, 5, 3], [9, 10, 3]]
+ 
+ 
+ 
+# printing original list
+print("The original list is : " + str(matriz))
+ 
+# Removing duplicates in Matrix
+# using loop
+matriz_sin_duplicar = []
+lista_compare = [] #declaramos una list para almacenar en paralelo todos los elementos de nuestra matriz resultante y que así sea más sencillo comparar
+index = 0 #necesitamos una variable para saber las filas que vamos insertando en nuestra matriz resultante
 
-#         lista_nueva.append(elem)
-#     matriz_nueva.append(lista_nueva)
-
-
-# print(lista_nueva)
+for lista in matriz: #nos quedamos con cada fila de la matriz
+    matriz_sin_duplicar.append([]); # por cada fila de la matriz añadiremos una lista vacía a nuestra matriz a devolver
+    for elem in lista: #por cada elemento de la fila de la matriz original
+        
+        #la idea será a la vez que ir creando nuestra matriz con los elementos por cada fila de la original
+        #ir metiendo en una lista sencilla esos mismos elementos, de modo que comparemos con la lista sencilla si ya están
+        #en lugar de con nuestra matriz
+        
+        if elem not in lista_compare:
+            matriz_sin_duplicar[index].append(elem)
+            lista_compare.append(elem)
+    index += 1
+ 
+# printing result
+print("The Matrix after duplicates removal is : " + str(matriz_sin_duplicar))
 
 
 # temps = [[1,2,3], [5,7], [1], [5,2,9]]
@@ -74,14 +90,14 @@ print (sin_duplicados(["verde","azul","gris","azul","rojo","negro","rojo"])) """
 #     print(key, "->", dictionary[key]
 
 
-lista = [1,2,4,5,6,7,2,3,3,4,4]
-diccionario = {}
-for elem in lista:
-    if elem in diccionario:
-        diccionario[elem]+=1
-    else:
-        diccionario[elem]=1
+# lista = [1,2,4,5,6,7,2,3,3,4,4]
+# diccionario = {}
+# for elem in lista:
+#     if elem in diccionario:
+#         diccionario[elem]+=1
+#     else:
+#         diccionario[elem]=1
         
-print (diccionario)
+# print (diccionario)
     
     
